277cadc64e5dcf8d316db5ea2fdbeb635294d8ce
Samuel Onuoha <156164112+sammyh3ndrix@users.noreply.github.com>
2026-09-22 22:06:51 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        anas = {}
+        anat = {}
+        for i in range(len(s)):
+            if s[i] in anas:
+                anas[s[i]] = anas[s[i]] + 1
+            else:
+                anas[s[i]] = 1
+            if t[i] in anat:
+                anat[t[i]] = anat[t[i]] + 1
+            else:
+                anat[t[i]] = 1
+        return anat == anas
+
+        
